a715d611be88efb887e2c44e55a16530d952464f
Legoking907 <144159788+Legoking907@users.noreply.github.com>
2025-01-29 18:58:36 -0900

    Added A control.py file for the python-based control sytem. It only has a basic psudo code structure with comments at the moment.

---
+++ control.py
@@ -0,0 +1,39 @@
+
+
+
+
+########## startup ##########
+
+# do some happy beeps
+
+########## Main Loop ##########
+
+# main loop
+
+    # find human
+
+    # if found human, do follow loop
+
+    # follow loop
+    
+        # follow tracked human until set time or other parameters
+
+        # once human is out of tracking parameters, wander for ~1 minute, then exit loop
+
+    # if no human is found after x time, do idle loop
+
+    # idle loop
+
+        # move to a wall and face cammera into the room.
+
+        # slow camera fps and initiate other low power mode settings, wait for signs of a human
+
+        # exit loop if human is detected
+
+    # exit main loop after shutdown command.
+    # LOW BATTERY: if low battery is detected, move to the nearest wall and shutdown.
+
+
+########## Shut Down ##########
+
+# do some sleepy beeps
